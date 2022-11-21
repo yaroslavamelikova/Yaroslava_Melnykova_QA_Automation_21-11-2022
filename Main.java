7baf75faa800f78dc719955f6a63ae6f2e22a589
Yaroslava Melnykova <yaroslava.melnikova.1410@gmail.com>
2022-11-21 17:16:54 -0500

    lesson_02

---
+++ Main.java
@@ -0,0 +1,11 @@
+package lesson_01;
+
+public class Main {
+    public static void main (String[] args) {
+
+        System.out.println("Hello world");
+        System.out.println("New line 01");
+        System.out.println("New line 02");
+        System.out.println("New line 03");
+    }
+}
